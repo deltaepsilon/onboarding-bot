d7215186460469acde632c67099b547170a8d291
Chris Esplin <chris@christopheresplin.com>
2025-06-26 20:03:16 +0000

    Repaire NEXT_PUBLIC_SLACK_SCOPES

--- src/lib/slack/config.ts
+++ src/lib/slack/config.ts
@@ -7,7 +7,7 @@ export const slackConfig = {
     clientId: process.env.SLACK_CLIENT_ID,
     clientSecret: process.env.SLACK_CLIENT_SECRET,
     stateSecret: process.env.SLACK_STATE_SECRET,
-    scopes: process.env.SLACK_SCOPES,
+    scopes: process.env.NEXT_PUBLIC_SLACK_SCOPES,
     appUrl: process.env.NEXT_PUBLIC_APP_URL,
 };
 
@@ -18,7 +18,7 @@ const ENV_VAR_NAMES: { [K in keyof typeof slackConfig]: string } = {
     clientId: 'SLACK_CLIENT_ID',
     clientSecret: 'SLACK_CLIENT_SECRET',
     stateSecret: 'SLACK_STATE_SECRET',
-    scopes: 'SLACK_SCOPES',
+    scopes: 'NEXT_PUBLIC_SLACK_SCOPES',
     appUrl: 'NEXT_PUBLIC_APP_URL',
 };
 
